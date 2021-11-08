e45ae6dac5eb84fd91e196e6d2a7f3f8e08c65f2
igo-l <igolllf@gmail.com>
2021-11-08 23:38:15 +0300

    feat: solve  05 merge styles add

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const directoryFirst = path.join(__dirname, "styles");
+const directorySecond = path.join(__dirname, "project-dist");
+const filesCopyPath = path.join(directoryFirst, "bundle.css");
+fs.readdir(directoryFirst, { withFileTypes: true }, function (err, files) {
+    fs.writeFile(filesCopyPath, "", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        files.forEach(function (file) {
+            if (file.isFile() && path.extname(file.name) == ".css") {
+                let path = directoryFirst + "/" + file.name;
+                fs.readFile(path, 'utf8' , (err, data) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    fs.appendFile(filesCopyPath, data, function (err) {
+                        if (err) throw err;
+                        console.log('Styles merged');
+                    });
+                })
+            }
+        })
+    });
+});
---
+++ 06-build-page/index.js
@@ -0,0 +1,6 @@
+
+const fs = require('fs');
+const path = require('path');
+const distPath = path.resolve(__dirname, 'project-dist');
+const sourcePath =  path.resolve(__dirname, 'styles');
+const bundlePath =  path.resolve(distPath, 'bundle.css');
